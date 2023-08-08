c47c125b00d60722ac7d553b4ace68c3af653e61
moleonlips <thanhlong.dev.1810@gmail.com>
2023-08-09 00:15:33 +0700

    feature add a new room into database.

--- src/controllers/room.controller.js
+++ src/controllers/room.controller.js
@@ -1,12 +1,12 @@
 const connection = require("../configs/database.config");
-const bodyParser = require("body-parser");
-
+const path = require("path");
 const getAllRooms = (req, res) => {
   // logic handlers
   let rooms = [];
 
   connection.query(
-    'SELECT * FROM room',
+    'SELECT * FROM room WHERE room.id = ?',
+    [1],
     (err, result) => {
       rooms = result;
       return res.render('rooms/home.ejs', { list: rooms });
@@ -15,15 +15,23 @@ const getAllRooms = (req, res) => {
 }
 
 const saveRoom = (req, res) => {
-  console.log('>>> room name: ', req.body.room);
-  console.log('>>> room length: ', req.body.length);
-  return res.render(
-    'rooms/save-success.ejs',
-    {
-      roomName: req.body.room,
-      roomLength: req.body.length
+
+  const room = req.body.room;
+  const length = Number(req.body.length);
+  const width = Number(req.body.width);
+  const height = Number(req.body.height);
+  const capacity = Number(req.body.capacity);
+  const area = length * width;
+
+  connection.query(
+    'INSERT INTO room (`room_name`, `room_length`, `room_width`, `room_height`, `room_area`, `max_capacity`) VALUES (?, ?, ?, ?, ?, ?);',
+    [room, length, width, height, area, capacity],
+    (err, result) => {
+      if (err) throw new Error('some thing went wrong: ', err);
+      console.log(`>>> result: `, result);
+      return res.render('room/save-success.ejs');
     }
-  );
+  )
 }
 
 const createANewRoom = (req, res) => {
