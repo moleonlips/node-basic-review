609eeeed64c9cec4eca1630af0a7d5447a7bea1c
moleonlips <thanhlong.dev.1810@gmail.com>
2023-08-11 17:14:00 +0700

    restruct folder tree ensure the project following mtf MVC patern

--- src/controllers/room.controller.js
+++ src/controllers/room.controller.js
@@ -1,41 +1,16 @@
-const connection = require("../configs/database.config");
-const path = require("path");
 const roomServices = require("../services/room.service");
 
 const getAllRooms = async (req, res) => {
-  // connection.query(
-  //   roomServices.getAllRooms,
-  //   (err, result) => {
-  //     if (err) throw new Error(`Something went wrong: ${err}`);
-
-  //     console.log('>> result: ', result)
-  //     return res.render('rooms/home.ejs', { list: result });
-  //     res.send(200, result);
-  //   }
-  // )
 
   /// connect db using PROMISE WRAPPER
-  const [result, fields] = await connection.query(`SELECT * FROM room`);
-  console.log('>>> check result: ', result);
+  const result = await roomServices.getAllRooms();
   return res.render('rooms/home.ejs', { data: result })
 }
 
 const getRoomByID = async (req, res) => {
   console.log('>>> req.params.id: ', req.params.id)
   const roomID = req.params.id;
-
-  // connection.query(
-  //   roomServices.getRoomByID, [roomID],
-  //   (err, result) => {
-  //     if (err) throw new Error(`Something went wrong: ${err}`);
-
-  //     console.log('>> result: ', result)
-  //     res.send(200, result);
-  //     // return res.render('rooms/home.ejs', { list: result });
-  //   }
-  // )
-
-  const [result, feilds] = await connection.query(roomServices.getRoomByID, [roomID]);
+  const result = await roomServices.getRoomByID(roomID);
   res.send(200, result);
 }
 
@@ -48,21 +23,9 @@ const saveRoom = async (req, res) => {
   const capacity = Number(req.body.capacity);
   const area = length * width;
 
-  // connection.query(
-  //   roomServices.addANewRoom,
-  //   [room, length, width, height, area, capacity],
-  //   (err, result) => {
-  //     if (err) throw new Error('some thing went wrong: ', err.message);
-  //     // res.send(200, 'Insert successfully!')
-  //     return res.render('rooms/save-success.ejs');
-  //   }
-  // )
 
   /// MYSQL2 PROMISS WRAPPER
-  await connection.query(
-    roomServices.addANewRoom,
-    [room, length, width, height, area, capacity]
-  )
+  roomServices.addANewRoom(room, length, width, height, capacity, area);
   console.log('>>> A new room was inserted: ', req.body);
   await res.render('rooms/save-success.ejs', { newRoom: req.body, message: 'Insert successfully!' });
 }
@@ -78,17 +41,7 @@ const updateRoomInfo = async (req, res) => {
   const roomID = req.params.id;
   console.log(`>>> room id: `, roomID);
 
-  // connection.query(
-  //   roomServices.updateRoom,
-  //   [room, length, width, height, area, capacity, roomID],
-  //   (err, result) => {
-  //     if (err) throw new Error('Something went wrong: ', err.message);
-  //     req.body = { ...req.body, roomID: roomID }
-  //     res.send(200, `Room has been updated successfully: ${req.body}`);
-  //   }
-  // )
-
-  await connection.query(roomServices.updateRoom, [room, length, width, height, area, capacity, roomID]);
+  await roomServices.updateRoom(room, length, width, height, area, capacity, roomID);
   // res.send(200, `Room ${roomID} was updated: ${req.body}`);
 
   await res.render('rooms/save-success.ejs', { newRoom: req.body, message: 'Update successfully!' });
@@ -99,16 +52,7 @@ const deleteRoom = async (req, res) => {
 
   console.log('>> room id: ', req.params)
 
-  // connection.query(
-  //   roomServices.deleteRoom,
-  //   [roomID],
-  //   (err, result) => {
-  //     if (err) throw new Error(`Something went wrong: ${err.message}`);
-  //     res.send(200, `Room was deleted: ${result}`);
-  //   }
-  // )
-
-  await connection.query(roomServices.deleteRoom, [roomID])
+  await roomServices.deleteRoom(roomID);
   console.log(`Rome ${roomID} was deleted!`);
   await res.render('rooms/save-success.ejs', { message: 'That room was deleted successfully!', newRoom: [] });
 }
@@ -118,20 +62,23 @@ const createANewRoom = (req, res) => {
 }
 
 const searchRoom = async (req, res) => {
-  const [result, fields] = await connection.query(
-    roomServices.searchRooms, [req.params.keysearch]
-  )
+  const keysearch = req.params.keysearch;
+  const result = await roomServices.searchRooms(keysearch);
   if (result.length) res.send(200, result);
   else res.send(400, `Have not any room with that name!`);
 }
 
 const getUpdateForm = async (req, res) => {
-  const [result, fields] = await connection.query(roomServices.getRoomByID, [req.params.id]);
+  const roomID = req.params.id;
+  const result = await roomServices.getRoomByID(roomID);
+
+  console.log('>>> check room: ', result);
   await res.render('rooms/update-form.ejs', { roomUpdate: result[0] });
 }
 
 const getDeleteConfirm = async (req, res) => {
-  const [result] = await connection.query(roomServices.getRoomByID, [req.params.id]);
+  const roomID = req.params.id;
+  const result = await roomServices.getRoomByID(roomID);
   await res.render('rooms/delete-confirm.ejs', { myRoom: result[0], message: 'Do you want to DELETE this room?' });
 }
 
--- src/services/room.service.js
+++ src/services/room.service.js
@@ -1,23 +1,39 @@
-const getAllRooms = `SELECT * FROM room`;
+const connection = require("../configs/database.config");
 
-const getRoomByID = `SELECT * FROM room WHERE room.id = ?`;
+const getAllRooms = async () => {
+  const [result] = await connection.query(`SELECT * FROM room`);
+  return result && result.length > 0 ? result : {};
+}
 
-const addANewRoom = `INSERT INTO room (room_name, room_length, room_width, room_height, room_area, max_capacity) VALUES (?, ?, ?, ?, ?, ?)`;
+
+const getRoomByID = async (roomid) => {
+  const [result] = await connection.query(`SELECT * FROM room WHERE room.id = ?`, [roomid]);
+  return result ? result : {};
+};
+
+const addANewRoom = async (room, length, width, height, area, capacity) => {
+  await connection.query(
+    `INSERT INTO room (room_name, room_length, room_width, room_height, room_area, max_capacity) VALUES (?, ?, ?, ?, ?, ?)`,
+    [room, length, width, height, area, capacity]
+  );
+};
 
 /// update room's info function, list of params need to pass (room name, length, width, height, area, max_capacity and id of the room is updating.)
-const updateRoom =
-  `UPDATE room SET 
-    room_name = ?,
-    room_length = ?,
-    room_width = ?,
-    room_height = ?,
-    room_area = ?,
-    max_capacity = ?
-  WHERE room.id = ?`;
+const updateRoom = async (room, length, width, height, area, capacity, roomID) => {
+  await connection.query(
+    `UPDATE room SET room_name = ?, room_length = ?, room_width = ?, room_height = ?, room_area = ?, max_capacity = ? WHERE room.id = ?`,
+    [room, length, width, height, area, capacity, roomID]
+  );
+};
 
-const deleteRoom = `DELETE FROM room WHERE room.id = ?`;
+const deleteRoom = async (roomid) => {
+  await connection.query(`DELETE FROM room WHERE room.id = ?`, [roomid]);
+};
 
-const searchRooms = `SELECT * FROM room r WHERE r.room_name LIKE CONCAT ('%', ?, '%')`;
+const searchRooms = async (keysearch) => {
+  const [result] = connection.query(`SELECT * FROM room r WHERE r.room_name LIKE CONCAT ('%', ?, '%')`, [keysearch]);
+  return result && result.length ? result : [];
+};
 
 module.exports = {
   getAllRooms,
